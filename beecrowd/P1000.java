898120cd8a0e8e2863264bfc78a59e42b08bff6b
Victor Aranha <aldo.victor1610@gmail.com>
2024-03-31 02:44:26 -0300

    beecrowd 1000

---
+++ beecrowd/P1000.java
@@ -0,0 +1,15 @@
+// Bem-vindo ao beecrowd!
+
+// O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+// Entrada
+// Este problema não possui nenhuma entrada.
+
+// Saída
+// Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+
+public class P1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
